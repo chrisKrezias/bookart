b96f30bec5f2033809a25bacb0676a045766542a
chris.krezias <chris.krezias@gmail.com>
2018-03-15 16:39:50 +0200

    1st commit

---
+++ js/controllers.js
@@ -0,0 +1,10 @@
+HeaderApp.controller('HeaderCtrl', function($scope) {
+    $scope.HeaderDetails = {
+        title: "BooKart",
+        tagline: "We have 1 million books for you"
+    };
+    $scope.NavDetails = {
+    	tag01: "Books",
+    	tag02: "Kart"
+    };
+});
---
+++ js/modules.js
@@ -0,0 +1,1 @@
+var HeaderApp = angular.module('HeaderApp', []);
